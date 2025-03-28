f7d4e443126736132dadc71a1318c79462aedf2b
zzyzzy <zzyzigy@gmail.com>
2025-03-28 09:48:16 +0900

    JPA로 구현한 게시판 본문글보기 API

---
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/domain/BoardReplyDTO.java
@@ -0,0 +1,18 @@
+package com.example.zzyzzy.semiprojectv2.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BoardReplyDTO {
+    private Board bd;
+    private List<?> rps;
+
+    public BoardReplyDTO(Board bd, List<?> rps) {
+        this.bd = bd;
+        this.rps = rps;
+    }
+}
+
+
