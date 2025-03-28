b81d79abbcecae56cb536e5da9b51b9a5062badb
zzyzzy <zzyzigy@gmail.com>
2025-03-28 11:40:22 +0900

    JPA로 구현한 게시판 조회수 증가 구현

--- SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/repository/BoardRepository.java
+++ SemiProjectV2/src/main/java/com/example/zzyzzy/semiprojectv2/repository/BoardRepository.java
@@ -5,7 +5,9 @@ import com.example.zzyzzy.semiprojectv2.domain.BoardDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -25,5 +27,9 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<BoardDTO> findByTitleContainsOrContentsContains(Pageable pageable, String fkey1, String fkey2);
 
     Board findByBno(Long bno);
+
+    @Modifying
+    @Query("update Board set views = views + 1 where bno = :bno")
+    int updateViews(@Param("bno") Long bno);
 
 }
